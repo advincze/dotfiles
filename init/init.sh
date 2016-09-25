463321aec6bc4d5fe5ec185ab641a826d3a5a7db
adam <advincze@gmail.com>
2016-09-25 09:30:29 +0200

    added iterm2 imagetools

--- init/init.sh
+++ init/init.sh
@@ -34,5 +34,5 @@ go get -u -v github.com/kardianos/govendor
 go get -u -v github.com/cespare/reflex
 go get -u -v github.com/davecgh/go-spew/spew #golang struct pretty printer lib
 go get -u -v github.com/pkg/errors
-
+go get -u -v github.com/olivere/iterm2-imagetools/...
 ./osx.sh
