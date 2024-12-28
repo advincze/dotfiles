af6424a98aca4b2b34108e1b66f5f5b405b1bfcf
Adam <a77055@eon.com>
2024-12-28 16:15:41 +0100

    use single script

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This is a shell script that defines the configuration
+# for which dotfiles to back up/install.
+#
+# List the files/folders (relative to your $HOME) that you
+# want to manage. For example:
+#
+#   .zshrc
+#   .config/karabiner/karabiner.json
+#   .gitignore_global
+#
+
+DOTFILES=(
+  ".zshrc"
+  ".devbox.zsh"
+  ".theme.zsh"
+  ".gitignore_global"
+  ".config/karabiner/karabiner.json"
+)
