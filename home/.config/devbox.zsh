53cf55bc522017c92c264c79ec9fa6769fb45381
Adam <advincze@gmail.com>
2025-04-28 00:18:02 +0200

    Update devbox.zsh

--- home/.config/devbox.zsh
+++ home/.config/devbox.zsh
@@ -1,5 +1,5 @@
 # activate devbox global
-eval "$(devbox global shellenv --init-hook)"
+eval "$(devbox global shellenv --init-hook --install --no-refresh-alias --omit-nix-env=true)"
 
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
